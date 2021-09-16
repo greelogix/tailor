28a38ce5ea5ce30856ecba66ced04e05d1769c3b
Abdullah's Macbook Air <abdullah@Abdullahss-MacBook-Air.local>
2021-09-16 14:49:38 +0500

    fix

--- js/custom.js
+++ js/custom.js
@@ -396,3 +396,23 @@ $(".hotspot").on("mouseover", function(e){
     	$(".tablink").removeClass("border-bottom")
 			$(this).addClass("border-bottom")
 	});
+
+
+	// product-detail-slider
+
+	jQuery(document).ready(function($) {
+
+	var owl = $("#owl-demo-2");
+  owl.owlCarousel({
+      items : 3, 
+      itemsDesktop : [992,3],
+      itemsDesktopSmall : [768,2], 
+      itemsTablet: [480,2], 
+      itemsMobile : [320,1]
+  });
+  $(".next").click(function(){ owl.trigger('owl.next'); });
+  $(".prev").click(function(){ owl.trigger('owl.prev'); });
+
+$('.latest-blog-posts .thumbnail.item').matchHeight();
+	
+});
