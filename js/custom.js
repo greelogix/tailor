f24a90964affaac2b66f5a08d3dc4f6cc06ac990
abdullahafzal-greelogix <abdullahafzal@greelogix.com>
2022-02-11 18:10:59 +0500

    changes

--- js/custom.js
+++ js/custom.js
@@ -628,3 +628,28 @@ function openPage(pageName,elmnt,color) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+
+
+
+
+
+
+
+
+
+let $id = (id) => document.getElementById(id);
+var [login, register, form] = ['login', 'register', 'form'].map(id => $id(id));
+
+[login, register].map(element => {
+    element.onclick = function () {
+        [login, register].map($ele => {
+            $ele.classList.remove("active");
+        });
+        this.classList.add("active");
+        this.getAttribute("id") === "register"?  form.classList.add("active") : form.classList.remove("active");
+    }
+});
+
+
+
