413e5438a1f39bbe8d458acc49f2ca081c81ab52
abdullahafzal-greelogix <abdullahafzal@greelogix.com>
2022-03-22 14:59:41 +0500

    changes

--- js/custom.js
+++ js/custom.js
@@ -603,23 +603,28 @@ function openPage(pageName,elmnt,color) {
 
 
 function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+  if( evt.currentTarget ){
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " " +"active";  
+  }  
 }
 
 // Get the element with id="defaultOpen" and click on it
 // document.getElementById("defaultOpen").click();
 
 
+ const h =document.querySelectorAll('#Home')
+ h.forEach(e => e.style.display='block')
+  
 
 // product detail tabs
 
@@ -662,10 +667,68 @@ var [login, register, form] = ['login', 'register', 'form'].map(id => $id(id));
         this.getAttribute("id") === "register"?  form.classList.add("active") : form.classList.remove("active");
       }
     }catch(ex){
-    console.log( ex );
+
     }
   
 });
 
+// let defaultOpen=document.querySelectorAll(
+//   '#defaultOpen')
+// defaultOpen.forEach(e => e.style.display='block')
+
+
+
+
+
+// dots
+
+
+const lerp = (a, b, n) => (1 - n) * a + n * b;
+
+class Cursor {
+  constructor() {
+    // config
+    this.target = { x: 0.5, y: 0.5 }; // mouse position
+    this.cursor = { x: 0.5, y: 0.5 }; // cursor position
+    this.speed = 0.2;
+    this.init();
+  }
+  bindAll() {
+    ["onMouseMove", "render"].forEach((fn) => (this[fn] = this[fn].bind(this)));
+  }
+  onMouseMove(e) {
+    //get normalized mouse coordinates [0, 1]
+    this.target.x = e.clientX / window.innerWidth;
+    this.target.y = e.clientY / window.innerHeight;
+    // trigger loop if no loop is active
+    if (!this.raf) this.raf = requestAnimationFrame(this.render);
+  }
+  render() {
+    //calculate lerped values
+    this.cursor.x = lerp(this.cursor.x, this.target.x, this.speed);
+    this.cursor.y = lerp(this.cursor.y, this.target.y, this.speed);
+    document.documentElement.style.setProperty("--cursor-x", this.cursor.x);
+    document.documentElement.style.setProperty("--cursor-y", this.cursor.y);
+    //cancel loop if mouse stops moving
+    const delta = Math.sqrt(
+      Math.pow(this.target.x - this.cursor.x, 2) +
+      Math.pow(this.target.y - this.cursor.y, 2)
+    );
+    if (delta < 0.001) {
+      cancelAnimationFrame(this.raf);
+      this.raf = null;
+      return;
+    }
+    //or continue looping if mouse is moving
+    this.raf = requestAnimationFrame(this.render);
+  }
+  init() {
+    this.bindAll();
+    window.addEventListener("mousemove", this.onMouseMove);
+    this.raf = requestAnimationFrame(this.render);
+  }
+}
+
+new Cursor();
 
 
